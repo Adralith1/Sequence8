0fde4dcb3061d87c574286106e52272fc5bedf02
Legru Florent <florent_legru@ens.univ-artois.fr>
2018-03-18 14:09:43 +0100

    Fonction recherche

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,3 +1,10 @@
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class BinaryTree {
     private BinaryTree leftChildren;
     private BinaryTree rightChildren;
@@ -50,33 +57,120 @@ public class BinaryTree {
     {
         return value;
     }
-    public void recherche()
+    public int hauteur(BinaryTree binaryTree)
     {
-
-    }
-    public void printTree(BinaryTree bTree)
-    {
-        if(bTree!=null)
+        if(binaryTree==null)
         {
-            printTree(bTree.leftChildren);
-            printTree(bTree.rightChildren);
-            System.out.print(bTree.value+ " ");
+            return 0;
         }
-
+        else
+            return (1+Math.max(hauteur(binaryTree.getLeftChildren()),hauteur(binaryTree.getRightChildren())));
     }
 
+    public ArrayList<String> getTreeDatas(BinaryTree bTree,int hauteur,int niveau, ArrayList<String> datas)
+    {
+        if(bTree==null)
+        {
+            datas.add(hauteur + " " + "X");
+            return null;
+        }
+        datas.add(hauteur + " " + bTree.value);
+        getTreeDatas(bTree.leftChildren,hauteur+1,niveau,datas);
+        getTreeDatas(bTree.rightChildren,hauteur+1,niveau,datas);
+        return datas;
+    }
+
+    public void printHashmapDatas(ArrayList<String> treeDatas)
+    {
+        String firstChar="";
+        int firstCharInt=0;
+        ArrayList<Integer> tabDeint=new ArrayList<Integer>();
+        ArrayList<String> arbreLigneParLignes=new ArrayList<String>();
+        boolean newLine=false;
+        treeDatas.sort(String::compareToIgnoreCase);
+        String tempStr="";
+        for(int i=0;i<treeDatas.size();i++)
+        {
+            firstChar = treeDatas.get(i).substring(0, 1);
+            firstCharInt = Integer.parseInt(firstChar);
+            tabDeint.add(firstCharInt);
+        }
+       for(int i=0;i<treeDatas.size();i++) {
+           firstChar = treeDatas.get(i).substring(0, 1);
+           firstCharInt = Integer.parseInt(firstChar);
+
+           if(firstCharInt!=tabDeint.get(i+1) ) {newLine=true;}
+           else newLine=false;
+           tabDeint.add(firstCharInt);
+           tempStr += "\t";
+           tempStr+=treeDatas.get(i).subSequence(1, treeDatas.get(i).length());
+           tempStr+="\t";
+           if(newLine)
+           {
+               tempStr+="\n";
+           }
+           arbreLigneParLignes.add(tempStr);
+           tempStr="";
+       }
+       int cpt=0;
+        for(int i=0;i<arbreLigneParLignes.get((arbreLigneParLignes.size()-1)).length()/2;i++)
+            System.out.print("\t");
+       for(int i=0;i<arbreLigneParLignes.size();i++) {
+           if(arbreLigneParLignes.get(i)!="\t")
+           {
+               for(int j=0;j<i;j++)
+               {
+                   cpt++;
+               }
+           }
+           System.out.print(arbreLigneParLignes.get(i));
+           if(cpt%2==0)
+           {
+               System.out.print("\t");
+           }
+           cpt=0;
+       }
+    }
+
+    public static boolean recherche(BinaryTree binaryTree, int valueToLookFor)
+    {
+        boolean found=false;
+        if(binaryTree.value==valueToLookFor) {
+            found=true;
+        }
+        else
+            {
+                if (binaryTree.value<valueToLookFor)
+                {
+                    if(binaryTree.rightChildren!=null)
+                       return recherche(binaryTree.rightChildren,valueToLookFor);
+                    else
+                        found=false;
+                }
+                else
+                {
+                    if(binaryTree.leftChildren!=null)
+                    {
+                        return recherche(binaryTree.leftChildren,valueToLookFor);
+                    }
+                    else
+                        found=false;
+                }
+            }
+            return found;
+    }
 
     public static void main(String args[])
     {
+        ArrayList<String> datas=new ArrayList<String>();
         BinaryTree binaryTree=new BinaryTree(65);
         binaryTree.insertion(40);
         binaryTree.insertion(80);
         binaryTree.insertion(30);
         binaryTree.insertion(48);
         binaryTree.insertion(75);
-        binaryTree.printTree(binaryTree);
-
-
-
+        datas=binaryTree.getTreeDatas(binaryTree,binaryTree.hauteur(binaryTree),0,datas);
+        //binaryTree.printHashmapDatas(datas);
+        System.out.println(recherche(binaryTree,22));
     }
 }
